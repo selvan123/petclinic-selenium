abf5dc82b1c04bed27733306066549e2a7cdeef9
bm-sushma <39586586+bm-sushma@users.noreply.github.com>
2019-02-22 17:12:13 +0530

    Update PetClinic.java

--- src/bm/petclinic/PetClinic.java
+++ src/bm/petclinic/PetClinic.java
@@ -1,7 +1,7 @@
 package bm.petclinic;
 
 import java.util.concurrent.TimeUnit;
-
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
