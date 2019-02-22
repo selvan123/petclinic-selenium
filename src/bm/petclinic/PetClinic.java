27f7138d99a15bcc41197442aeb93d9d5f4a5c29
bm-sushma <39586586+bm-sushma@users.noreply.github.com>
2019-02-22 16:56:15 +0530

    Update PetClinic.java

--- src/bm/petclinic/PetClinic.java
+++ src/bm/petclinic/PetClinic.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
-
+import java.util.List;
 public class PetClinic {
 	public static WebDriver driver;
 	@BeforeSuite
@@ -43,4 +43,35 @@ public class PetClinic {
 		//driver.findElement(By.xpath("//div[@id='main-navbar']/ul/li[3]/a")).click();
 		
 	}
+	
+	@Test
+	public void addOwner(){
+	driver.findElement(By.linkText("FIND OWNERS")).click();
+		 driver.findElement(By.linkText("Add Owner")).click();
+		 driver.findElement(By.xpath("//form[@class='form-horizontal']//input[@name='firstName']")).sendKeys("Demo");
+		 driver.findElement(By.xpath("//form[@class='form-horizontal']//input[@name='lastName']")).sendKeys("Test");
+		 driver.findElement(By.xpath("//form[@class='form-horizontal']//input[@name='address']")).sendKeys("Dummy address");
+		 driver.findElement(By.xpath("//form[@class='form-horizontal']//input[@name='city']")).sendKeys("Mumbai");
+		 driver.findElement(By.xpath("//form[@class='form-horizontal']//input[@name='telephone']")).sendKeys("1234567890");
+		 driver.findElement(By.xpath("//form[@class='form-horizontal']//button[@class='btn btn-default']")).click();
+		 
+	}
+	
+	@Test
+	public void findOwner(){
+	driver.findElement(By.linkText("FIND OWNERS")).click();
+		 driver.findElement(By.xpath("//form[@id='search-owner-form']//input[@name='lastName']")).sendKeys("Test");
+		 driver.findElement(By.xpath("//form[@id='search-owner-form']//button[@class='btn btn-default']")).click();
+	}
+	
+	@Test
+	public void findVet(){
+	 driver.findElement(By.linkText("VETERINARIANS")).click();
+		 List<WebElement> list=driver.findElements(By.xpath("//table[@class='table table-striped']//td"));
+		 System.out.println("Available Vets with Specialities");
+		 for(WebElement iterate:list) {
+			 System.out.println(iterate.getText());
+		 }
+	}
+	
 }
