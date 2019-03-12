6b4c47da6e21995b9e9eed297a224f4cd86f58db
bm-sushma <39586586+bm-sushma@users.noreply.github.com>
2019-03-12 11:36:48 +0530

    Update PetClinic.java

--- src/bm/petclinic/PetClinic.java
+++ src/bm/petclinic/PetClinic.java
@@ -10,6 +10,15 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 import java.util.List;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import org.apache.poi.sl.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 public class PetClinic {
 	public static WebDriver driver;
 	@BeforeSuite
@@ -46,13 +55,16 @@ public class PetClinic {
 	
 	@Test
 	public void addOwner(){
+	PetClinic p1 =new PetClinic();
+	List<String> data=p1.readExcel();
 	driver.findElement(By.linkText("FIND OWNERS")).click();
 		 driver.findElement(By.linkText("Add Owner")).click();
-		 driver.findElement(By.xpath("//form[@class='form-horizontal']//input[@name='firstName']")).sendKeys("Demo");
-		 driver.findElement(By.xpath("//form[@class='form-horizontal']//input[@name='lastName']")).sendKeys("Test");
-		 driver.findElement(By.xpath("//form[@class='form-horizontal']//input[@name='address']")).sendKeys("Dummy address");
-		 driver.findElement(By.xpath("//form[@class='form-horizontal']//input[@name='city']")).sendKeys("Mumbai");
-		 driver.findElement(By.xpath("//form[@class='form-horizontal']//input[@name='telephone']")).sendKeys("1234567890");
+		  driver.findElement(By.xpath("//form[@class='form-horizontal']//input[@name='firstName']")).sendKeys(data.get(0));
+		 driver.findElement(By.xpath("//form[@class='form-horizontal']//input[@name='lastName']")).sendKeys(data.get(1));
+		 driver.findElement(By.xpath("//form[@class='form-horizontal']//input[@name='address']")).sendKeys(data.get(2));
+		 driver.findElement(By.xpath("//form[@class='form-horizontal']//input[@name='city']")).sendKeys(data.get(3));
+		 driver.findElement(By.xpath("//form[@class='form-horizontal']//input[@name='telephone']")).sendKeys(data.get(4));
+		 
 		 driver.findElement(By.xpath("//form[@class='form-horizontal']//button[@class='btn btn-default']")).click();
 		 
 	}
@@ -74,4 +86,33 @@ public class PetClinic {
 		 }
 	}
 	
+	public List<String> readExcel() {
+		List<String> data = new ArrayList<String>();
+		File file=new File("C:\\Users\\maudas\\Desktop\\PetClinic_DataGenerator.xlsx");
+		try {
+			FileInputStream inputStream = new FileInputStream(file);
+			Workbook tdmWorkBook=new XSSFWorkbook(inputStream);
+			org.apache.poi.ss.usermodel.Sheet tdmSheet=tdmWorkBook.getSheet("PetClinic");
+			int rowCount=tdmSheet.getLastRowNum()-tdmSheet.getFirstRowNum();
+			for (int i = 1; i < rowCount+1; i++) {
+
+		        Row row = tdmSheet.getRow(i);
+		        for (int j = 0; j < row.getLastCellNum(); j++) {
+		        	String field=row.getCell(j).getStringCellValue();
+					data.add(field);
+			}
+			}
+			/*for(String data1:data) {
+				System.out.println(data1);
+			}*/
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return data;
+	}
 }
