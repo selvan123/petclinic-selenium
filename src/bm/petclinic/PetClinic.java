09f8c6efea1e437049ac2ea24fd32c3f87e601e7
selvan123 <selvans@gmail.com>
2019-03-20 16:26:38 +0530

    Update PetClinic.java

--- src/bm/petclinic/PetClinic.java
+++ src/bm/petclinic/PetClinic.java
@@ -30,7 +30,7 @@ public class PetClinic {
 		driver=new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(300, TimeUnit.SECONDS);
-		driver.get("http://54.214.95.28:8081/Petclinic/");
+		driver.get("http://54.214.95.28:8081/petclinic/");
 		//String url=driver.findElement(By.xpath("//body")).getText();
 		//driver.get(url);
 		
